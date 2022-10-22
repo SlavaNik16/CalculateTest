2d97c237176609007d7751fd76fb3a94a694ca31
SlavaNik16 <70444635+SlavaNik16@users.noreply.github.com>
2022-10-22 15:17:16 +0300

    Add files via upload

---
+++ Calculate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите первое число: ");
+            var cal1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            var cal2 = int.Parse(Console.ReadLine());
+
+            var cal = new LibCal.Class1();
+
+            Console.WriteLine($"Сумма двух чисел {cal1} + {cal2} = {cal.Sum(cal1, cal2)}");
+
+        }
+    }
+}
---
+++ LibCal/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LibCal
+{
+    public class Class1
+    {
+        public int Sum(int val1, int val2)
+        {
+            if (val1 > 50 || val1 < 0)
+            {
+                throw new ArgumentOutOfRangeException("val1",val1,
+                    "Значение операнда 1 должно быть в пределах 0..50");
+            }
+            if (val2 < 50 || val2 > 100)
+            {
+                throw new ArgumentOutOfRangeException("val2", val2,
+                    "Значение операнда 1 должно быть в пределах 50..100");
+            }
+            return val1 + val2;
+        }
+    }
+}
